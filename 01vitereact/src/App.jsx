7871571739e9ef2f4d4fa99a468fa77561a2da51
Rohit Gupta <r2614476@gmail.com>
2025-01-13 21:13:56 +0000

    repo update

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,15 @@
 import Chai from "./chai"
 
 function App() {
+const username = "chai aur code"
+
     return (
        <>    
     <Chai /> 
-    <h1>welcome {"<>"} called fragment here</h1>
+    <h1>welcome {"<>"} called fragment here {username}</h1>
     </>
   )
 }
 
 export default App
+// {usename}--it is called evaluated expression
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,13 +2,51 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 
+function MyApp(){
+        return(
+                <div>
+                        <h1>Custom App!</h1>
+                </div>
+        )
+}
 
+// const ReactElement = {
+//         type: 'a',
+//         props: {
+//             href:'https://google.com',
+//             target: '_blank'
+//         },
+//         children: 'Click me to visit google'
+//     }
+
+
+const AnotherElement = (
+       <a href="https://google.com" target='_blank'>Visit Google </a>
+)
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+        'a',
+        {href: 'https://google.com', target: '_blank'},
+        'click me to visut google',
+        anotherUser
+         
+)
 ReactDOM.createRoot(document.getElementById('root')).
 render(
    
-        <App />
+      reactElement
    
 )
 
+// in React.createElement(
+//         1st it html Element(a,h1,package,...)
+//         2nd it is properties that we want to show
+//         3rd is the children part 
+//         4th is the rest variable name
+// )
+// bable inject this reactElement.reactElement

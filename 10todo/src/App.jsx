a4245da2eec21a14516b702257a1a79428984861
rohit <rohitraj6203199@gmail.com>
2025-01-23 19:38:16 +0530

    updated

---
+++ 10todo/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Todoprovider } from './context'
+import { useEffect } from 'react'
+
+function App() {
+  const [todos,setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev)=>[{id:Date.now(),...todo},...prev])
+  }
+
+  const updateTodo = (id,todo)=>{
+       setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id)=>{
+    setTodos((prev) => prev.filter((todo) => todo.id !== id) )
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ?  {...prevTodo, completed: !prevTodo.completed}: prevTodo))
+  }
+  // local storage work on browser if you go to any server then browser local storage will not haandle this.
+  useEffect(()=>{
+    //  localStorage.getItem("todos") these all values are in stirng so we want in json format
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length>0){
+       setTodos(todos)
+    }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos])
+
+  return (
+    <Todoprovider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
+    <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+    </Todoprovider>
+  )
+}
+
+export default App
